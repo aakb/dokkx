2b32fc43a81d90c6e6b038053aea7baf3cfac783
Martin Yde Granath <my@aarhus.dk>
2016-09-23 10:10:54 +0200

    Added siteimprove

--- sites/all/themes/dokkx/js/site-improve.js
+++ sites/all/themes/dokkx/js/site-improve.js
@@ -2,5 +2,4 @@
   var sz = document.createElement('script'); sz.type = 'text/javascript'; sz.async = true;
   sz.src = '//ssl.siteimprove.com/js/siteanalyze_2240844.js';
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(sz, s);
-  {color:#1F497D}
 })();
