f5503ebd1a2c3176ece4516064437afec3a204f8
Martin Yde Granath <my@aarhus.dk>
2016-09-23 09:53:42 +0200

    sites

--- sites/all/themes/dokkx/js/menu-toggle.js
+++ sites/all/themes/dokkx/js/menu-toggle.js
@@ -4,43 +4,9 @@
  *
  */
 
-(function ($) {
-  "use strict";
-
-  var hamburger_button = $('.nav-toggle');
-  var hamburger_menu = $('.hamburger-menu');
-  var html = $('html');
-  var body = $('body');
-  var overlay = $('.overlay');
-
-  $('.js-menu-toggle').click(function() {
-    if(hamburger_button.hasClass("open")){
-      // Button animation 'back to hamburger'.
-      hamburger_button.removeClass("open");
-
-      // Closes hamburger menu.
-      hamburger_menu.removeClass("is-open");
-
-      // Hides overlay.
-      overlay.removeClass('is-visible');
-
-      // Unlocks html and body element.
-      html.removeClass('is-locked');
-      body.removeClass('is-locked');
-    }
-    else {
-      // Hamburger button animatiion to 'x'.
-      hamburger_button.addClass("open");
-
-      // Open hamburger menu.
-      hamburger_menu.addClass("is-open");
-
-      // Shows overlay.
-      overlay.addClass('is-visible');
-
-      // Lock html and body elements.
-      html.addClass('is-locked');
-      body.addClass('is-locked');
-    }
-  });
-})(jQuery);
+(function() {
+  var sz = document.createElement('script'); sz.type = 'text/javascript'; sz.async = true;
+  sz.src = '//ssl.siteimprove.com/js/siteanalyze_2240844.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(sz, s);
+  {color:#1F497D}
+})();
